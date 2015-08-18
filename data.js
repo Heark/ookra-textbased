18ab01d1bc7e94baa08aeff1f5f816508bfdecaf
Heark <hearkofsorcerey@gmail.com>
2015-08-18 17:43:17 -0400

    Create data.js

---
+++ data.js
@@ -0,0 +1,62 @@
+
+// Connecting to a session
+var Session = new Candy.session()
+Session.call()
+var OS = Session.ENV()
+// Ok lets get started with the actual code
+
+
+
+
+// can we even run it on here? Link?
+// no we're running it in jsbin
+// http://jsbin.com/vojedujuyi/edit?html,css,js,output
+// READY
+
+//You alive there?
+var get_name = prompt("What is your name?");
+
+var Players = function(hp, name) {
+    this.hp = hp;
+    this.name = name;
+    // anything else you can think of
+};
+var Item = function(item, desc, effect){
+    this.item = item;
+    this.desc = desc; 
+    this.effect = effect;
+};
+var Enemies = function(name, hp, drop){
+    this.name = name;
+    this.hp = hp;
+    this.drop = drop; 
+};
+
+// Enemies
+var Slime = new Enemies("Slime", 20, drop_1)
+
+
+// Player
+var Player = new Players(100, get_name);
+
+// Items
+var Potion = new Item("Potion", "Recovers hp by 20 pts", 20);
+
+// ENEMY Drop arrays
+var drop_1 = [Potion] 
+
+// Inventory
+var Inventory = [];
+
+
+var companion = function(species, name){
+    this.type = animal;
+    this.name = name;
+};
+
+var Griffon = new companion("Griffon", "Grif"); 
+var Talking_Potato = new companion("Talking Potato", "Hotness");
+var Slime = new companion("Slime","Jiggles");
+var Dog = new companion("Dog","Doug"); // Doug LOL
+
+// end of data.js
