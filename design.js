2675b84f2cebce95d8e725e5cdde854a17794a6e
Heark <hearkofsorcerey@gmail.com>
2015-08-20 00:52:39 -0400

    Create design.js

---
+++ design.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+  $(".start-button").click(function(){
+     $(this).css("background", "#D3D3D3");
+  });
+  $(".start-button").hover(function(){
+       $(this).css('border-radius', "25px");
+  });
+});
