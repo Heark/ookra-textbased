9b046e278277c2d43c796bdfa09ea544f9a771b0
Heark <hearkofsorcerey@gmail.com>
2015-08-20 01:03:11 -0400

    Update Main.js

--- Main.js
+++ Main.js
@@ -1,2 +1,16 @@
 /*
 */
+
+// Buttons below
+$(document).ready(function() {
+    $(".start-button").click(function() {
+        $(this).css("background", "#D3D3D3");
+    });
+    $(".start-button").hover(function() {
+        $(this).css('border-radius', "25px");
+    }, function() {
+        $(this).css('border-radius', "0px");
+    });
+});
+
+// End of Buttons
