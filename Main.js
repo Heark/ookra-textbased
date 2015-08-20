a129a23a51418af90c24d431c34007967aafb6ae
Heark <hearkofsorcerey@gmail.com>
2015-08-20 14:58:02 -0400

    Update Main.js

--- Main.js
+++ Main.js
@@ -2,14 +2,19 @@
 */
 
 // Buttons below
+
+// Start Game Button
 $(document).ready(function() {
-    $(".start-button").click(function() {
-        $(this).css("background", "#D3D3D3");
+    $('.start-button').click(function() {
+        $(this).css('background', '#D3D3D3');
     });
-    $(".start-button").hover(function() {
-        $(this).css('border-radius', "25px");
-    }, function() {
-        $(this).css('border-radius', "0px");
+    $('.start-button').hover((function() {
+        $(this).css('border-radius', '25px');
+        $(this).css('border', '1px solid');
+        $(this).css('border-color', '#00FFFF');
+    }), function() {
+        $(this).css('border-radius', '0px');
+        $(this).css('border', '0px solid');
     });
 });
 
