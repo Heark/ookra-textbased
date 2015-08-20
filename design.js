34ab1c6b07a64625902ded33e5143f93337927c4
Heark <hearkofsorcerey@gmail.com>
2015-08-20 01:01:19 -0400

    Update design.js

--- design.js
+++ design.js
@@ -1,8 +1,10 @@
-$(document).ready(function(){
-  $(".start-button").click(function(){
-     $(this).css("background", "#D3D3D3");
-  });
-  $(".start-button").hover(function(){
-       $(this).css('border-radius', "25px");
-  });
+$(document).ready(function() {
+    $(".start-button").click(function() {
+        $(this).css("background", "#D3D3D3");
+    });
+    $(".start-button").hover(function() {
+        $(this).css('border-radius', "25px");
+    }, function() {
+        $(this).css('border-radius', "0px");
+    });
 });
