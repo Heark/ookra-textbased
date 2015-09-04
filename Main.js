a818ed55f56b8830b8e5a4e0210bafe2959162b9
Heark <hearkofsorcerey@gmail.com>
2015-09-03 22:12:59 -0400

    Update Main.js

--- Main.js
+++ Main.js
@@ -7,6 +7,7 @@
 $(document).ready(function() {
     $('.start-button').click(function() {
         $(this).css('background', '#D3D3D3');
+        startGame();
     });
     $('.start-button').hover((function() {
         $(this).css('border-radius', '25px');
@@ -19,3 +20,6 @@ $(document).ready(function() {
 });
 
 // End of Buttons
+function startGame(){
+    document.write("Test")
+}
